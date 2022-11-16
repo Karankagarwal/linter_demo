8ffd725c6ba25eff825d76b6fb6dbf8161911e2b
karankagarwal <karan.agarwal@bluecore.com>
2022-11-16 19:33:09 +0530

    Demo Starts from Here

--- tests/test_app.py
+++ tests/test_app.py
@@ -3,6 +3,6 @@ from src.app import AppDemo
 
 class TestApp:
 
-    """Test A """
+    """Test A"""
     def test_Typechecking_and_naming_convention(self):
         print(f'\n output is {AppDemo(10).Typechecking_and_naming_convention(2,3)}')
